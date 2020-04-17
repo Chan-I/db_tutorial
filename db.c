d7a797fcba08a17b8393044001c9e6d86779b4a6
zyjiang <foxiyiyi12081003@gmail.com>
2020-04-17 16:28:49 +0800

    modify const  to #define and pass all make file

--- db.c
+++ db.c
@@ -47,7 +47,7 @@ typedef struct {
 } Statement;
 
 #define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
-
+/*
 const uint32_t ID_SIZE = size_of_attribute(Row, id);
 const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
 const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
@@ -55,9 +55,20 @@ const uint32_t ID_OFFSET = 0;
 const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
 const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
 const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+*/
 
-const uint32_t PAGE_SIZE = 4096;
+#define ID_SIZE sizeof(((Row*)0)->id)
+#define USERNAME_SIZE sizeof(((Row*)0)->username)
+#define EMAIL_SIZE sizeof(((Row*)0)->email)
+#define ID_OFFSET 0
+#define USERNAME_OFFSET (ID_OFFSET + ID_SIZE)
+#define EMAIL_OFFSET (USERNAME_OFFSET + USERNAME_SIZE)
+#define ROW_SIZE (ID_SIZE + USERNAME_SIZE + EMAIL_SIZE)
+#define  PAGE_SIZE 4096
 #define TABLE_MAX_PAGES 100
+
+
+
 
 typedef struct {
   int file_descriptor;
@@ -87,6 +98,7 @@ typedef enum { NODE_INTERNAL, NODE_LEAF } NodeType;
 /*
  * Common Node Header Layout
  */
+/*
 const uint32_t NODE_TYPE_SIZE = sizeof(uint8_t);
 const uint32_t NODE_TYPE_OFFSET = 0;
 const uint32_t IS_ROOT_SIZE = sizeof(uint8_t);
@@ -95,10 +107,20 @@ const uint32_t PARENT_POINTER_SIZE = sizeof(uint32_t);
 const uint32_t PARENT_POINTER_OFFSET = IS_ROOT_OFFSET + IS_ROOT_SIZE;
 const uint8_t COMMON_NODE_HEADER_SIZE =
     NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_POINTER_SIZE;
+*/
+
+#define NODE_TYPE_SIZE sizeof(uint8_t)
+#define NODE_TYPE_OFFSET  0
+#define IS_ROOT_SIZE sizeof(uint8_t)
+#define IS_ROOT_OFFSET NODE_TYPE_SIZE
+#define PARENT_POINTER_SIZE sizeof(uint32_t)
+#define PARENT_POINTER_OFFSET ((IS_ROOT_OFFSET)+(IS_ROOT_SIZE))
+#define COMMON_NODE_HEADER_SIZE ((NODE_TYPE_SIZE)+(IS_ROOT_SIZE)+(PARENT_POINTER_SIZE))
 
 /*
  * Internal Node Header Layout
  */
+/*
 const uint32_t INTERNAL_NODE_NUM_KEYS_SIZE = sizeof(uint32_t);
 const uint32_t INTERNAL_NODE_NUM_KEYS_OFFSET = COMMON_NODE_HEADER_SIZE;
 const uint32_t INTERNAL_NODE_RIGHT_CHILD_SIZE = sizeof(uint32_t);
@@ -107,20 +129,43 @@ const uint32_t INTERNAL_NODE_RIGHT_CHILD_OFFSET =
 const uint32_t INTERNAL_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE +
                                            INTERNAL_NODE_NUM_KEYS_SIZE +
                                            INTERNAL_NODE_RIGHT_CHILD_SIZE;
+*/
+#define INTERNAL_NODE_NUM_KEYS_SIZE  sizeof(uint32_t)
+#define INTERNAL_NODE_NUM_KEYS_OFFSET  COMMON_NODE_HEADER_SIZE
+#define INTERNAL_NODE_RIGHT_CHILD_SIZE  sizeof(uint32_t)
+#define INTERNAL_NODE_RIGHT_CHILD_OFFSET ((INTERNAL_NODE_NUM_KEYS_OFFSET) + (INTERNAL_NODE_NUM_KEYS_SIZE))
+#define INTERNAL_NODE_HEADER_SIZE ((COMMON_NODE_HEADER_SIZE) + (INTERNAL_NODE_NUM_KEYS_SIZE) + (INTERNAL_NODE_RIGHT_CHILD_SIZE))
+
+
 
 /*
  * Internal Node Body Layout
  */
+/*
 const uint32_t INTERNAL_NODE_KEY_SIZE = sizeof(uint32_t);
 const uint32_t INTERNAL_NODE_CHILD_SIZE = sizeof(uint32_t);
 const uint32_t INTERNAL_NODE_CELL_SIZE =
     INTERNAL_NODE_CHILD_SIZE + INTERNAL_NODE_KEY_SIZE;
+*/
 /* Keep this small for testing */
+/*
 const uint32_t INTERNAL_NODE_MAX_CELLS = 3;
+*/
+
+
+
+#define INTERNAL_NODE_KEY_SIZE  sizeof(uint32_t)
+#define INTERNAL_NODE_CHILD_SIZE  sizeof(uint32_t)
+#define INTERNAL_NODE_CELL_SIZE ((INTERNAL_NODE_CHILD_SIZE) + (INTERNAL_NODE_KEY_SIZE))
+/* Keep this small for testing */
+
+#define INTERNAL_NODE_MAX_CELLS  3
+
 
 /*
  * Leaf Node Header Layout
  */
+/*
 const uint32_t LEAF_NODE_NUM_CELLS_SIZE = sizeof(uint32_t);
 const uint32_t LEAF_NODE_NUM_CELLS_OFFSET = COMMON_NODE_HEADER_SIZE;
 const uint32_t LEAF_NODE_NEXT_LEAF_SIZE = sizeof(uint32_t);
@@ -129,10 +174,20 @@ const uint32_t LEAF_NODE_NEXT_LEAF_OFFSET =
 const uint32_t LEAF_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE +
                                        LEAF_NODE_NUM_CELLS_SIZE +
                                        LEAF_NODE_NEXT_LEAF_SIZE;
+*/
 
+
+
+#define LEAF_NODE_NUM_CELLS_SIZE  sizeof(uint32_t)
+#define LEAF_NODE_NUM_CELLS_OFFSET  COMMON_NODE_HEADER_SIZE
+#define LEAF_NODE_NEXT_LEAF_SIZE  sizeof(uint32_t)
+#define LEAF_NODE_NEXT_LEAF_OFFSET ((LEAF_NODE_NUM_CELLS_OFFSET) + (LEAF_NODE_NUM_CELLS_SIZE))
+#define LEAF_NODE_HEADER_SIZE  ((COMMON_NODE_HEADER_SIZE) + (LEAF_NODE_NUM_CELLS_SIZE) + (LEAF_NODE_NEXT_LEAF_SIZE))
 /*
  * Leaf Node Body Layout
  */
+
+/*
 const uint32_t LEAF_NODE_KEY_SIZE = sizeof(uint32_t);
 const uint32_t LEAF_NODE_KEY_OFFSET = 0;
 const uint32_t LEAF_NODE_VALUE_SIZE = ROW_SIZE;
@@ -145,6 +200,19 @@ const uint32_t LEAF_NODE_MAX_CELLS =
 const uint32_t LEAF_NODE_RIGHT_SPLIT_COUNT = (LEAF_NODE_MAX_CELLS + 1) / 2;
 const uint32_t LEAF_NODE_LEFT_SPLIT_COUNT =
     (LEAF_NODE_MAX_CELLS + 1) - LEAF_NODE_RIGHT_SPLIT_COUNT;
+*/
+
+#define LEAF_NODE_KEY_SIZE sizeof(uint32_t)
+#define LEAF_NODE_KEY_OFFSET 0
+#define LEAF_NODE_VALUE_SIZE ROW_SIZE
+#define LEAF_NODE_VALUE_OFFSET ((LEAF_NODE_KEY_OFFSET) + (LEAF_NODE_KEY_SIZE))
+#define LEAF_NODE_CELL_SIZE ((LEAF_NODE_KEY_SIZE) + (LEAF_NODE_VALUE_SIZE))
+#define LEAF_NODE_SPACE_FOR_CELLS ((PAGE_SIZE) - (LEAF_NODE_HEADER_SIZE))
+#define LEAF_NODE_MAX_CELLS ((LEAF_NODE_SPACE_FOR_CELLS) / (LEAF_NODE_CELL_SIZE))
+#define LEAF_NODE_RIGHT_SPLIT_COUNT ((LEAF_NODE_MAX_CELLS + 1) / 2)
+#define LEAF_NODE_LEFT_SPLIT_COUNT ((LEAF_NODE_MAX_CELLS + 1) - LEAF_NODE_RIGHT_SPLIT_COUNT)
+
+
 
 NodeType get_node_type(void* node) {
   uint8_t value = *((uint8_t*)(node + NODE_TYPE_OFFSET));
